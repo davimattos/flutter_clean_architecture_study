03c7a84a91a0f5c6a83d9e45fe5a20569c4f0724
mattosdavi <davi.mattos@thoughtworks.com>
2021-08-02 15:06:24 -0300

    change Authentication usecase to receive a body

--- lib/domain/usecases/authenticaton.dart
+++ lib/domain/usecases/authenticaton.dart
@@ -2,6 +2,12 @@ import 'package:flutter_clean_architecture/domain/entities/account_entity.dart';
 import 'package:meta/meta.dart';
 
 abstract class Authentication {
-  Future<AccountEntity> auth(
-      {@required String email, @required String password});
+  Future<AccountEntity> auth(AuthenticationParams params);
+}
+
+class AuthenticationParams {
+  final String? email;
+  final String? password;
+
+  AuthenticationParams({@required this.email, @required this.password});
 }
