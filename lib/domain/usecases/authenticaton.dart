febc391a2bca93c18f4d7c335fc6776e333f9997
Davi Mattos <davimattos@MacBook-Pro-de-Davi.local>
2021-08-02 14:27:21 -0300

    ensure RemoteAuthentication calls HttpClient with correct URL

--- lib/domain/usecases/authenticaton.dart
+++ lib/domain/usecases/authenticaton.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_clean_architecture/domain/entities/account_entity.dart';
+import 'package:meta/meta.dart';
 
 abstract class Authentication {
-  Future<AccountEntity> auth({required String email, required String password});
+  Future<AccountEntity> auth(
+      {@required String email, @required String password});
 }
