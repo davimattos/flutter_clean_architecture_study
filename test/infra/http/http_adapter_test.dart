a5e91464e40406dcc601c66c8c6b14e93dbb550d
mattosdavi <davi.mattos@thoughtworks.com>
2021-08-16 13:53:57 -0300

    move HttpAdapter to its own file

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,36 +1,16 @@
-import 'dart:convert';
-
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-import 'package:flutter_clean_architecture/data/http/http.dart';
+import 'package:flutter_clean_architecture/infra/http/http.dart';
 
 class ClientSpy extends Mock implements Client {}
-
-class HttpAdapter implements HttpClient {
-  final Client client;
-
-  HttpAdapter(this.client);
-
-  Future<Map> request(
-      {required String url, required String method, Map? body}) async {
-    final headers = {
-      'content-type': 'application/json',
-      'accept': 'application/json'
-    };
-    final jsonBody = body != null ? jsonEncode(body) : null;
-    final response =
-        await client.post(Uri.parse(url), headers: headers, body: jsonBody);
-    return response.body.isEmpty ? null : jsonDecode(response.body);
-  }
-}
 
 void main() {
   late HttpAdapter sut;
   late ClientSpy client;
-  late final String url;
+  late String url;
 
   setUp(() {
     client = ClientSpy();
@@ -39,8 +19,8 @@ void main() {
   });
 
   group('post', () {
-    PostExpectation mockRequest() =>
-        when(client.post(Uri.parse(url), headers: anyNamed('headers')));
+    PostExpectation mockRequest() => when(client.post(Uri.parse(url),
+        headers: anyNamed('headers'), body: anyNamed('body')));
 
     void mockResponse(int statusCode,
         {String body = '{"any_key": "any_value"}'}) {
@@ -85,6 +65,14 @@ void main() {
 
     test('Should return null if post returns 204', () async {
       mockResponse(200, body: '');
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
+    });
+
+    test('Should return null if post returns 204 with data', () async {
+      mockResponse(200);
 
       final response = await sut.request(url: url, method: 'post');
 
