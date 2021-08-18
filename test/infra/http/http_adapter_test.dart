cc3393d6e458d048a275873d8df3c9a15c0fa273
mattosdavi <davi.mattos@thoughtworks.com>
2021-08-18 16:07:10 -0300

    ensure HttpAdapter throws ServerError if invalid method is provided

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -19,6 +19,14 @@ void main() {
     url = faker.internet.httpUrl();
   });
 
+  group('shared', () {
+    test('Should throw ServerError if invalid method is provided', () async {
+      final future = await sut.request(url: url, method: 'invalid_method');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
+  });
+
   group('post', () {
     PostExpectation mockRequest() => when(client.post(Uri.parse(url),
         headers: anyNamed('headers'), body: anyNamed('body')));
