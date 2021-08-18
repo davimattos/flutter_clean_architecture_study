e151964a6f9627da7e695b985534b2c3365614a2
mattosdavi <davi.mattos@thoughtworks.com>
2021-08-18 16:10:24 -0300

    ensure HttpAdapter throws ServerError if post throws

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -36,6 +36,10 @@ void main() {
       mockRequest().thenAnswer((_) async => Response(body, 200));
     }
 
+    void mockError() {
+      mockRequest().thenThrow(Exception());
+    }
+
     setUp(() {
       mockResponse(200);
     });
@@ -130,6 +134,14 @@ void main() {
 
     test('Should return ServerError if post returns 500', () async {
       mockResponse(500);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
+
+    test('Should return ServerError if post throws', () async {
+      mockError();
 
       final future = sut.request(url: url, method: 'post');
 
