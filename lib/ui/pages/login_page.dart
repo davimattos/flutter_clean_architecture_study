fd90982e873a3261b7198f7044f8e02e76e1b0ff
mattosdavi <davi.mattos@thoughtworks.com>
2021-08-30 11:10:21 -0300

    add login page layout

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,51 +1,66 @@
 import 'package:flutter/material.dart';
 
+import '../components/components.dart';
+
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              child: Image(
-                image: AssetImage('lib/ui/assets/logo.png'),
-              ),
+            LoginHeader(),
+            Headline1(
+              text: 'Login',
             ),
-            Text('Login'.toUpperCase()),
-            Form(
-                child: Column(
-              children: <Widget>[
-                TextFormField(
-                  decoration: InputDecoration(
-                      labelText: 'Email', icon: Icon(Icons.email)),
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                      labelText: 'Senha', icon: Icon(Icons.lock)),
-                  obscureText: true,
-                ),
-                ElevatedButton(
-                    onPressed: null, child: Text('Entrar'.toLowerCase())),
-                SizedBox.fromSize(
-                  size: Size(100, 56),
+            Padding(
+              padding: const EdgeInsets.all(32),
+              child: Form(
                   child: Column(
-                    children: [
-                      TextButton(
-                        onPressed: () {},
-                        child: Row(
-                          children: <Widget>[
-                            Icon(Icons.person),
-                            Text('Criar Conta')
-                          ],
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      icon: Icon(Icons.email,
+                          color: Theme.of(context).primaryColorLight),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8, bottom: 32),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Senha',
+                        icon: Icon(
+                          Icons.lock,
+                          color: Theme.of(context).primaryColorLight,
                         ),
                       ),
-                    ],
+                      obscureText: true,
+                    ),
                   ),
-                )
-              ],
-            ))
+                  ElevatedButton(
+                      onPressed: null, child: Text('Entrar'.toLowerCase())),
+                  SizedBox.fromSize(
+                    size: Size(100, 56),
+                    child: Column(
+                      children: [
+                        TextButton(
+                          onPressed: () {},
+                          child: Row(
+                            children: <Widget>[
+                              Icon(Icons.person),
+                              Text('Criar Conta')
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              )),
+            )
           ],
         ),
       ),
